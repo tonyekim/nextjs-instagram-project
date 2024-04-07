7829fd437482d9858336a00fd95c51f2e2d03ec2
tonyekim <otoobongekim@gmail.com>
2024-04-07 15:10:29 +0100

    add modal functionality using react modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
 "use client";
-
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { signIn, useSession , signOut} from "next-auth/react";
+import { signIn, useSession, signOut } from "next-auth/react";
+import Modal from "react-modal";
+import { HiCamera, HiOutlinePlusCircle } from "react-icons/hi";
+import {AiOutlineClose} from "react-icons/ai";
 
 const Header = () => {
   const { data: session } = useSession();
+  const [modalIsOpen, setIsOpen] = useState(false);
 
-  console.log(session);
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -38,7 +44,19 @@ const Header = () => {
         {/* menu items */}
 
         {session ? (
-          <img src={session.user.image} alt={session.user.name} className="h-10 w-10 rounded-full cursor-pointer" onClick={() => signOut()} />
+          <div className="flex items-center gap-2">
+            <HiOutlinePlusCircle
+              size={28}
+              onClick={() => setIsOpen(true)}
+              className=" cursor-pointer transform hover:scale-125 transition duration-300 hover:text-red-600"
+            />
+            <img
+              src={session.user.image}
+              alt={session.user.name}
+              className="h-10 w-10 rounded-full cursor-pointer"
+              onClick={() => signOut()}
+            />
+          </div>
         ) : (
           <button
             onClick={() => signIn()}
@@ -48,6 +66,36 @@ const Header = () => {
           </button>
         )}
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+        onRequestClose={() => setIsOpen(false)}
+        ariaHideApp={false}
+      >
+        <div className="flex flex-col justify-center items-center h-[100%]">
+          <HiCamera className="text-5xl text-gray-400 cursor-pointer" />
+
+          <input
+            type="text"
+            maxLength="150"
+            placeholder="Please enter you caption..."
+            className="m-4 border-none text-center w-full focus:ring-0 outline-none"
+            // onChange={(e) => setCaption(e.target.value)}
+          />
+
+          <button
+            className="w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+          >
+            Upload Post
+          </button>
+
+          <AiOutlineClose
+            className='cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300'
+            onClick={() => setIsOpen(false)}
+          />
+        </div>
+      </Modal>
     </div>
   );
 };

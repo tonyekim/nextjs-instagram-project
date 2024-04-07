19eac716c6164cab0d208b32c297f07bfb4072f8
tonyekim <otoobongekim@gmail.com>
2024-04-07 14:06:26 +0100

    add next auth and modify the header componenet

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-
+import Header from "../components/Header";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,11 +12,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-      <Header />
-        
-        {children}</body>
-    </html>
+    <SessionWrapper>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+
+          {children}
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { signIn, useSession , signOut} from "next-auth/react";
 
 const Header = () => {
+  const { data: session } = useSession();
+
+  console.log(session);
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -25,10 +31,22 @@ const Header = () => {
         </Link>
         {/* search inputs */}
 
-        <input type="text" className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]" />
+        <input
+          type="text"
+          className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]"
+        />
         {/* menu items */}
 
-        <button className=" text-blue-500 font-semibold text-sm">Log In</button>
+        {session ? (
+          <img src={session.user.image} alt={session.user.name} className="h-10 w-10 rounded-full cursor-pointer" onClick={() => signOut()} />
+        ) : (
+          <button
+            onClick={() => signIn()}
+            className=" text-blue-500 font-semibold text-sm"
+          >
+            Log In
+          </button>
+        )}
       </div>
     </div>
   );
